35eee57c50d7260768e46798d7ff23044c8a4467
ndxbn <sitri.ndxbn@gmail.com>
2017-11-27 09:54:33 +0900

    mysql charset

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -11,4 +11,4 @@ cp /root/.my.cnf /home/vagrant/.my.cnf
 
 DB=$1;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8mb4_general_ci DEFAULT COLLATE utf8_general_ci";
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci";
