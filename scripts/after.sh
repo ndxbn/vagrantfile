94b6f1c147e17c575a48f3f1833ed127b418c852
ndxbn <sitri.ndxbn@gmail.com>
2017-11-28 16:00:34 +0900

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,1 +1,6 @@
 #!/bin/bash -eu
+
+# http://packagist.jp
+composer config -g repos.packagist composer https://packagist.jp
+# https://github.com/hirak/prestissimo
+composer global require hirak/prestissimo
